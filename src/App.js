3a9a596b2fa117e8d9ddc6bf200322b4dfb469e0
gonzam77 <gonzam77@gmial.com>
2024-01-03 16:15:19 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Home from './views/home/Home'
 
 
-export const urlDev = 'http://localhost:3001';
+export const urlDev = 'http://localhost:3000';
+
 //export const urlProduction = 'https://videogames-server-ypgw.onrender.com';
 //export const URL = urlProduction;
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <p>Hola!</p>
-      </header>
+    <div className='App'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
