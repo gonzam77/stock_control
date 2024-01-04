c2a2f60ac40079ce383bfaab69b3a1df6386bfa7
gonzam77 <gonzam77@gmail.com>
2024-01-04 09:47:16 -0300

    Components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
-import Home from './views/home/Home'
-import Nav from './components/nav/nav'
-
+import Home from './views/home/Home';
+import Nav from './components/nav/nav';
+import Clients from './views/clients/clients';
+import Suppliers from './views/suppliers/suppliers';
+import Dispatchers from './views/dispatchers/dispatchers';
+import ProductList from './views/productsList/productList';
+import ProductDetail from './views/productDetail/productDetail';
+import Users from './views/users/users';
+import About from './views/about/about';
+import Footer from './components/footer/footer';
 
 export const urlDev = 'http://localhost:3000';
 
@@ -20,7 +27,15 @@ function App() {
       <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/clients" element={<Clients />} />
+          <Route path="/suppliers" element={<Suppliers />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/dispatchers" element={<Dispatchers />} />
+          <Route path="/productList" element={<ProductList />} />
+          <Route path="/productDetail" element={<ProductDetail />} />
+          <Route path="/about" element={<About />} />
         </Routes>
+      <Footer />  
       </div>
     );
 }
