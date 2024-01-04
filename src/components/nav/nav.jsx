4dde2e595c1bdc41429d2db6457b3ea4b035dfbd
gonzam77 <gonzam77@gmial.com>
2024-01-04 07:43:48 -0300

    nav buttons

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,9 +1,19 @@
-import styles from './nav.module.css'
+import style from "./nav.module.css";
+import SearchBar from '../SearchBar/SearchBar.jsx';
+import { Link } from "react-router-dom";
 
-export default function Nav(){
+export default function Nav() {
     return (
-        <div className={styles.container}>
-            <>nav</>
+        <div className={style.nav}>
+            <Link to="/home">
+                <button className={style.home}>Home</button>
+            </Link>
+            <Link to="/form">
+                <button className={style.form}>Create</button>
+            </Link>
+            <div className={style.searchBar}>
+                <SearchBar className={style.searchBar} />
+            </div>
         </div>
     )
 }
