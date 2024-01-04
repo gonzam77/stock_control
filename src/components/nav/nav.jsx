0b1c21e280cecf8d728b3aa6ad5adfe64ae685c1
gonzam77 <gonzam77@gmail.com>
2024-01-04 09:10:49 -0300

    cards

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,18 +1,35 @@
 import style from "./nav.module.css";
-import SearchBar from '../SearchBar/SearchBar.jsx';
+//import SearchBar from '../SearchBar/SearchBar.jsx';
 import { Link } from "react-router-dom";
 
 export default function Nav() {
     return (
-        <div className={style.nav}>
-            <Link to="/home">
-                <button className={style.home}>Home</button>
-            </Link>
-            <Link to="/form">
-                <button className={style.form}>Create</button>
-            </Link>
-            <div className={style.searchBar}>
-                <SearchBar className={style.searchBar} />
+        <div>
+            <div className={style.nav}>
+                <Link to="/">
+                    <button className={style.button}>Home</button>
+                </Link>
+                <Link to="/products">
+                    <button className={style.button}>Productos</button>
+                </Link>
+                <Link to="/suppliers">
+                    <button className={style.button}>Proveedores</button>
+                </Link>
+                <Link to="/clients">
+                    <button className={style.button}>Clientes</button>
+                </Link>
+                <Link to="/users">
+                    <button className={style.button}>Usuarios</button>
+                </Link>
+                <Link to="/dispatchers">
+                    <button className={style.button}>Tramportistas</button>
+                </Link>
+                <Link to="/movements">
+                    <button className={style.button}>Movimientos</button>
+                </Link>
+                {/* <div className={style.searchBar}>
+                    <SearchBar className={style.searchBar} />
+                </div> */}
             </div>
         </div>
     )
