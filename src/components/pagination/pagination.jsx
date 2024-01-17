ae4a3ec6fb51ec21fa507d691b213dfedf883625
gonzam77 <gonzam77@gmail.com>
2024-01-17 10:43:23 -0300

    pagination

--- src/components/pagination/pagination.jsx
+++ src/components/pagination/pagination.jsx
@@ -1,56 +1,82 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Pagination from "react-bootstrap/Pagination";
+import Card from "../cards/card/card";
 import styles from "./pagination.module.css";
+import { useSelector } from "react-redux";
 
-export default function Paginate({
-  productsPerPage,
-  videogames,
-  paginate,
-  currentPage,
-}) {
-  const pageNumbers = [];
-
-  for (let i = 1; i <= Math.ceil(videogames / productsPerPage); i++) {
-    pageNumbers.push(i);
-  }
-
-  function handlePrev(event) {
-    if (currentPage > 1) {
-      paginate(currentPage - 1);
+function AdvancedExample() {
+  const products = useSelector((state) => state.products);
+  const itemsPerPage = 8; // Puedes ajustar esto según tus necesidades
+  const totalItems = products.length; // Puedes ajustar esto según tus necesidades
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const forrr = () => {
+    for (let i = 0; i < products.length; i++) { 
+      return(
+          <Card
+            id={products[i].id}
+            code={products[i].code}
+            name={products[i].name}
+            image={products[i].image}
+            price={products[i].price}
+            stock={products[i].stock}
+            marca={products[i].marca}
+            description={products[i].description}
+            supplier={products[i].supplier}
+          />
+      )    
     }
   }
-  function handleNext(event) {
-    if (currentPage < pageNumbers.length) {
-      paginate(currentPage + 1);
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    // Puedes realizar acciones adicionales cuando cambia la página, como cargar datos para la página actual
+  }, [currentPage]);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const renderPaginationItems = () => {
+    const pages = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pages.push(
+        <Pagination.Item
+          key={i}
+          active={i === currentPage}
+          onClick={() => handlePageChange(i)}
+        >
+          {i}
+        </Pagination.Item>
+      );
     }
-  }
+    return pages;
+  };
 
   return (
-    <div className={styles.container}>
-      {pageNumbers.length ? (
-        <button className={styles.prevPageButton} onClick={handlePrev}>
-          prev
-        </button>
-      ) : null}
-      {pageNumbers &&
-        pageNumbers.map((number, index) => {
-          return (
-            <button
-              key={index}
-              className={styles.paginate}
-              onClick={() => paginate(number)}
-            >
-              {number}
-            </button>
-            // <li key={index} className={styles.pagination}>
-            //     <a className={styles.page} href onClick={() => paginate(number)}>{number}</a>
-            // </li>
-          );
-        })}
-      {pageNumbers.length ? (
-        <button className={styles.nextPageButton} onClick={handleNext}>
-          next
-        </button>
-      ) : null}
-    </div>
+    <>
+      <div className={styles.container}>
+        <div className={styles.cards}>
+          {forrr()}
+        </div>
+      </div>
+      <Pagination>
+        <Pagination.First onClick={() => handlePageChange(1)} />
+        <Pagination.Prev
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        />
+
+        {renderPaginationItems()}
+
+        <Pagination.Next
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        />
+        <Pagination.Last onClick={() => handlePageChange(totalPages)} />
+      </Pagination>
+    </>
   );
 }
+
+export default AdvancedExample;
---
+++ src/components/pagination/paginationNative.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styles from "./Paginate.module.css"
+
+export default function PaginateNative({ productsPerPage, products, paginate, currentPage }) {
+
+    const pageNumbers = [];
+
+    for (let i = 1; i <= Math.ceil(products / productsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+
+    function handlePrev(event) {
+        if (currentPage > 1) {
+            paginate(currentPage - 1)
+        }
+    }
+    function handleNext(event) {
+        if (currentPage < pageNumbers.length) {
+            paginate(currentPage + 1)
+        }
+    }
+
+    return (
+        <div className={styles.container}>
+            {
+                pageNumbers.length ?
+                    <button className={styles.prevPageButton} onClick={handlePrev}>Prev</button> :
+                    null
+            }
+            {pageNumbers &&
+                pageNumbers.map((number, index) => {
+                    return (
+                        <button key={index} className={styles.paginate} onClick={() => paginate(number)}>{number}</button>
+                        // <li key={index} className={styles.pagination}>
+                        //     <a className={styles.page} href onClick={() => paginate(number)}>{number}</a>
+                        // </li>
+
+                    )
+                })}
+            {
+                pageNumbers.length ?
+                    <button className={styles.nextPageButton} onClick={handleNext}>Next</button> :
+                    null
+            }
+        </div>
+    )
+}
